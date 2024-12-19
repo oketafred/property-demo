7b58684eb0aa46593c4fb5615ff7797780eeb272
Oketa Fred <oketafred@gmail.com>
2024-12-19 09:30:08 +0300

    Refactor app layout and integrate Tailwind CSS
    - Updated app.vue to display a centered heading for "Properties 256 - Coming Soon".
    - Configured nuxt.config.ts to include Tailwind CSS and PostCSS setup.
    - Added main.css for Tailwind CSS styles.
    - Created tailwind.config.js for Tailwind CSS configuration.
    - Updated package.json and package-lock.json to include necessary dependencies for Tailwind CSS and PostCSS.

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl text-blue-500 font-bold underline">
+      Properties 256 - Coming Soon
+    </h1>
   </div>
 </template>
